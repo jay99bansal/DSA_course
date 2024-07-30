ddce5da2c9905a684673acdcf8357fc755c270f7
vishnuthejaj <vishnutheja@hotmail.com>
2024-07-30 17:15:07 +0530

    updated good numbers

--- Number_Theory/Almost_Prime.cpp
+++ Number_Theory/Almost_Prime.cpp
@@ -1,25 +1,8 @@
-/*
-#include<bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-#define FOR(i,a,b) for(int i=(a); i<(b); i++)
-#define FORk(i,a,b,k) for(int i=(a); i<(b); i+=k)
-#define RFOR(i,a,b) for(int i=(a); i>=(b); i--)
-#define RFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
-#define pb push_back
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef long long int ll;
-typedef unsigned long long int  ull;
-typedef vector<ll> vll;
-typedef vector<ull> vull;
-*/
-
 #include<iostream>
 #include<vector>
 using namespace std;
 
-const int N = 3000;
+const int N = 1000;
 // Stores smallest prime factor
 int sp[N+1];
 
--- Number_Theory/Cout_Good_Numbers.cpp
+++ Number_Theory/Cout_Good_Numbers.cpp
@@ -1,3 +1,4 @@
+/*
 const int mod = 1e9+7;
 
 long long pow(int a, long long b) {
@@ -15,3 +16,56 @@ public:
         return (pow(4, op)*pow(5, ep))%mod;
     }
 };
+*/
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    long long mod = 1000000007;
+    
+    long long power(long long x, long long y) {   
+        if (y == 0) return 1;
+        long long ans = power(x, y/2);
+        ans *= ans;
+        ans %= mod;
+        if (y % 2) ans *= x;
+        ans %= mod;
+        return ans;
+    }
+    
+    int countGoodNumbers(long long n) {
+        long long odd = n/2;
+        long long even = n/2 + n % 2;
+        return (power(5, even) * power(4, odd)) % mod;
+    }
+};
+
+int main() {
+    Solution s;
+    long long n = 50;
+    cout << s.countGoodNumbers(n);
+}
+
+//C++ 
+/*
+Problem Statement/Link: (HW)https://leetcode.com/problems/count-good-numbers/
+
+Example 1:
+Input: n = 1
+Output: 5
+Explanation: The good numbers of length 1 are "0", "2", "4", "6", "8".
+
+Example 2:
+Input: n = 4
+Output: 400
+
+Example 3:
+Input: n = 50
+Output: 564908303
+ 
+Constraints:
+1 <= n <= 10^15
+
+By Jay Bhansal
+*/
